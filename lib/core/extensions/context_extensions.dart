ac39cf10badcee77aa5fc521f1bf29748693552f
digsales <diogodobu@gmail.com>
2026-02-24 12:47:29 -0300

    feat(): added separeted safe paddings.

--- lib/core/extensions/context_extensions.dart
+++ lib/core/extensions/context_extensions.dart
@@ -9,6 +9,12 @@ extension BuildContextExtensions on BuildContext {
   Size get screenSize => MediaQuery.sizeOf(this);
   bool get isDark => Theme.of(this).brightness == Brightness.dark;
   AppLocalizations get l10n => AppLocalizations.of(this)!;
+  EdgeInsets get padding => MediaQuery.paddingOf(this);
+  double get paddingTop => MediaQuery.paddingOf(this).top;
+  double get paddingBottom => MediaQuery.paddingOf(this).bottom;
+  double get paddingLeft => MediaQuery.paddingOf(this).left;
+  double get paddingRight => MediaQuery.paddingOf(this).right;
+
   EdgeInsets get paddingScreen {
     final padding = MediaQuery.paddingOf(this);
     return EdgeInsets.only(
