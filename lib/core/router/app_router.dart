81db1899387b080984cbe198ff58dc7c7dfd7cbc
digsales <diogodobu@gmail.com>
2026-03-02 11:39:55 -0300

    feat(): added home page and auth and session into app routes.

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:nowly/core/services/auth_service_provider.dart';
+import 'package:nowly/features/home/home_screen.dart';
 
 import '../../features/forgot_password/forgot_password_screen.dart';
 import '../../features/onboarding/onboarding_screen.dart';
@@ -8,10 +10,14 @@ import '../../features/signin/signin_screen.dart';
 import '../../features/signup/signup_screen.dart';
 
 abstract class AppRoutes {
+  // auth routes
   static const String onboarding = '/';
   static const String signin = '/signin';
   static const String signup = '/signup';
   static const String forgotPassword = '/forgot-password';
+  
+  // authenticated routes
+  static const String home = '/home';
 }
 
 enum PageTransitionType {
@@ -46,9 +52,37 @@ CustomTransitionPage _buildPage(GoRouterState state, Widget child) {
 }
 
 final routerProvider = Provider<GoRouter>((ref) {
+  final authState = ref.watch(authStateProvider);
+
   return GoRouter(
     initialLocation: AppRoutes.onboarding,
+    redirect: (context, state) {
+      if (authState.isLoading) return null;
+
+      final user = authState.asData?.value;
+
+      final isLoggedIn = user != null;
+
+      final isAuthRoute =
+          state.fullPath == AppRoutes.signin ||
+          state.fullPath == AppRoutes.signup ||
+          state.fullPath == AppRoutes.forgotPassword ||
+          state.fullPath == AppRoutes.onboarding;
+
+      // Not logged - Only auth rotes
+      if (!isLoggedIn && !isAuthRoute) {
+        return AppRoutes.onboarding;
+      }
+
+      // Logged - Go to authenticated routes
+      if (isLoggedIn && isAuthRoute) {
+        return AppRoutes.home;
+      }
+
+      return null;
+    },
     routes: [
+      // auth routes
       GoRoute(
         path: AppRoutes.onboarding,
         pageBuilder: (context, state) =>
@@ -68,6 +102,13 @@ final routerProvider = Provider<GoRouter>((ref) {
         path: AppRoutes.forgotPassword,
         pageBuilder: (context, state) =>
             _buildPage(state, const ForgotPasswordPage()),
+      ),
+
+      // authenticated routes
+      GoRoute(
+        path: AppRoutes.home,
+        pageBuilder: (context, state) =>
+            _buildPage(state, const HomeScreen()),
       ),
     ],
   );
--- lib/core/services/auth_service_provider.dart
+++ lib/core/services/auth_service_provider.dart
@@ -5,3 +5,8 @@ import 'auth_service.dart';
 final authServiceProvider = Provider<AuthService>(
   (_) => AuthService(FirebaseAuth.instance),
 );
+
+final authStateProvider = StreamProvider<User?>((ref) {
+  final authService = ref.watch(authServiceProvider);
+  return authService.authStateChanges;
+});
