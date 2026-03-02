da63648d30391cdc015948085a13d566d000f65d
digsales <diogodobu@gmail.com>
2026-03-02 14:21:53 -0300

    style(): change fab style and added tooltip.

--- lib/features/home/home_shell.dart
+++ lib/features/home/home_shell.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:nowly/core/extensions/context_extensions.dart';
 import 'package:nowly/features/home/widgets/home_bottom_nav_bar.dart';
 
 class HomeShell extends StatelessWidget {
@@ -16,6 +17,9 @@ class HomeShell extends StatelessWidget {
         onPressed: () {
           // TODO: abrir tela de adicionar task
         },
+        tooltip: context.l10n.fabAddTask,
+        foregroundColor: context.colorScheme.onPrimary,
+        backgroundColor: context.colorScheme.primary,
         shape: const CircleBorder(),
         child: const Icon(Ionicons.add),
       ),
