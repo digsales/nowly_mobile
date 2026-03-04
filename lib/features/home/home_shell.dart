7ef2a795e68a2b0defa9aa1ac7fed0d049db21bc
Diogo Sales <diogodobu@gmail.com>
2026-03-04 15:36:53 -0300

    feat(): added levelup banner and level bar in home widgets.

--- lib/features/home/home_shell.dart
+++ lib/features/home/home_shell.dart
@@ -3,18 +3,59 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:nowly/core/extensions/context_extensions.dart';
-import 'package:nowly/core/widgets/user_level_bar.dart';
+import 'package:nowly/core/utils/level_utils.dart';
+import 'package:nowly/features/home/widgets/level_up_banner.dart';
+import 'package:nowly/features/home/widgets/user_level_bar.dart';
 import 'package:nowly/features/home/widgets/home_bottom_nav_bar.dart';
 import 'package:nowly/features/profile/profile_provider.dart';
 
-class HomeShell extends ConsumerWidget {
+
+class HomeShell extends ConsumerStatefulWidget {
   const HomeShell({super.key, required this.navigationShell});
 
   final StatefulNavigationShell navigationShell;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomeShell> createState() => _HomeShellState();
+}
+
+class _HomeShellState extends ConsumerState<HomeShell> {
+  OverlayEntry? _levelUpEntry;
+
+  void _showLevelUp(int newLevel) {
+    _levelUpEntry?.remove();
+    _levelUpEntry = null;
+
+    late final OverlayEntry entry;
+    entry = OverlayEntry(
+      builder: (_) => LevelUpBanner(
+        title: context.l10n.levelUpTitle(newLevel),
+        onDismiss: () {
+          entry.remove();
+          _levelUpEntry = null;
+        },
+      ),
+    );
+
+    _levelUpEntry = entry;
+    Overlay.of(context).insert(entry);
+  }
+
+  @override
+  void dispose() {
+    _levelUpEntry?.remove();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final userAsync = ref.watch(currentUserProvider);
+
+    ref.listen(currentUserProvider, (prev, next) {
+      final prevLevel = calculateLevel(prev?.asData?.value?.totalPoints ?? 0);
+      final nextLevel = calculateLevel(next.asData?.value?.totalPoints ?? 0);
+      if (nextLevel > prevLevel) _showLevelUp(nextLevel);
+    });
 
     return Scaffold(
       extendBody: true,
@@ -31,7 +72,7 @@ class HomeShell extends ConsumerWidget {
               indicatorColor: context.colorScheme.onPrimary,
             ),
           ),
-          Expanded(child: navigationShell),
+          Expanded(child: widget.navigationShell),
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -46,10 +87,10 @@ class HomeShell extends ConsumerWidget {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: HomeBottomNavBar(
-        activeIndex: navigationShell.currentIndex,
-        onTap: (index) => navigationShell.goBranch(
+        activeIndex: widget.navigationShell.currentIndex,
+        onTap: (index) => widget.navigationShell.goBranch(
           index,
-          initialLocation: index == navigationShell.currentIndex,
+          initialLocation: index == widget.navigationShell.currentIndex,
         ),
       ),
     );
